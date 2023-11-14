aa52ecd3b0b2cbc7ebe6bbcb517a42799ba034b3
rosscameron7 <rcamer211@caledonian.ac.uk>
2023-11-14 23:29:34 +0000

    Further improvement

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -12,7 +12,13 @@ class Dec2Hex
             return; // Exit the program
         }
 
-        Arg1 = Integer.parseInt(args[0]);
+        try {
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Input must be a valid integer. Please provide a decimal value.");
+            return; // Exit the program
+        }
+
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
@@ -27,6 +33,5 @@ class Dec2Hex
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-
     }
 }
