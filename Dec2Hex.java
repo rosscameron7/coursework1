90e7f6e5a7a1961d60949440ade9710610cc4216
rosscameron7 <rcamer211@caledonian.ac.uk>
2023-11-14 23:24:13 +0000

    improved java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,6 +7,11 @@ class Dec2Hex
 
     public static void main(String args[])
     {
+        if (args.length == 0) {
+            System.out.println("Error: No input argument provided. Please provide a decimal value.");
+            return; // Exit the program
+        }
+
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
