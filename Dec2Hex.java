d89a5bda5dba511211629905397641378f6fe778
rosscameron7 <rcamer211@caledonian.ac.uk>
2023-11-15 15:04:28 +0000

    more sq

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,15 +1,15 @@
 class Dec2Hex {
 
-    private static int Arg1;
-
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Error: No input argument provided. Please provide a value.");
             return;
         }
 
+        int num;
+
         try {
-            Arg1 = Integer.parseInt(args[0]);
+            num = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
             System.out.println("Error: Input must be a valid integer. Please provide a valid number.");
             return;
@@ -17,8 +17,6 @@ class Dec2Hex {
 
         char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int rem;
-        int num;
-        num = Arg1;
 
         StringBuilder hexadecimalBuilder = new StringBuilder();
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
