036c6cdb369018634ec772fd9b0fb67abcf8e2da
rosscameron7 <rcamer211@caledonian.ac.uk>
2023-11-15 14:13:22 +0000

    Implementing SonarQube Recs

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,39 +1,39 @@
-import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-class Dec2Hex
-{
+class Dec2Hex {
+
+    private static final Logger logger = LoggerFactory.getLogger(Dec2Hex.class);
 
     public static int Arg1;
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         if (args.length == 0) {
-            System.out.println("Error: No input argument provided. Please provide a decimal value.");
+            logger.error("Error: No input argument provided. Please provide a value.");
             return; // Exit the program
         }
 
         try {
             Arg1 = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println("Error: Input must be a valid integer. Please provide a decimal value.");
+            logger.error("Error: Input must be a valid integer. Please provide a valid number.");
             return; // Exit the program
         }
 
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int rem, num;
         num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        String hexadecimal = "";
+        logger.info("Converting the Decimal Value {} to Hex...", num);
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-        System.out.println("Thank you for using this converter!");
-        System.out.println("Ross Cameron - DevOps");
+        logger.info("Hexadecimal representation is: {}", hexadecimal);
+        logger.info("Thank you for using this converter!");
+        logger.info("Ross Cameron - DevOps");
     }
 }
